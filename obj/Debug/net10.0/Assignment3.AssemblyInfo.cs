b43113f1805c7479753252fec43e8a68cb316d69
Roman Lodhi <romanlodhi@gmail.com>
2026-06-21 18:36:18 -0400

    addfront now had new node set as head, and also point to the after it. and node after it points to the node before it.

--- obj/Debug/net10.0/Assignment3.AssemblyInfo.cs
+++ obj/Debug/net10.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a471d7e7cc060175d4507754e34c4832ece117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3254fde5cf0e69eee0533c4cdcbb80f9c7e877c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

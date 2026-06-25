0069cde99f4136d20993f1b2ec05e7ce4a15eae3
euniicee <126977294+euniicee@users.noreply.github.com>
2026-06-25 15:53:06 -0400

    Updated Cat.cs to match all stages of code

--- Cat.cs
+++ Cat.cs
@@ -23,6 +23,29 @@ public class Cat : Animal
         set { catBreed = value;}
 
     }
+
+    // This modifies SmellList with all animals that are within a 10 unit radius, and 
+    // checks for other predators in range
+    public void Smell(DoublyLinkedList<Animal> allAnimals, Bird[] birds, bool[] eaten)
+    {
+        SmellList = new DoublyLinkedList<Animal>();
+        // check other predators
+        Node<Animal>? curr = allAnimals.Head;
+        while (curr != null)
+        {
+            if (curr.data != this && FindDistance(curr.data) <= 10)
+                SmellList.AddLast(curr.data);
+            curr = curr.next;
+        }
+
+        // check birds
+        for (int i = 0; i < birds.Length; i++)
+        {
+            if (!eaten[i] && FindDistance(birds[i]) <= 10)
+                SmellList.AddLast(birds[i]);
+        }
+
+    }
 // custom ToString class for subclass Cat
     public override string ToString()
     {
